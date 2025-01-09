6b9b9e03445d34dfbd0cb0860d5bd53ee5167f15
Sareen Ahamed <sareenahamed40@gmail.com>
2025-01-09 23:45:29 +0530

    Tailwind updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
-        "daraz": "#fa5409",
+      colors: {
+        daraz: "#fa5409",
       },
-      fontFamily:{
+      fontFamily: {
         poppins: ["Poppins"],
       },
       animation: {
-        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeIn: "fadeIn 1s ease-in-out",
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
       },
     },
   },
   plugins: [
-    require('tailwind-scrollbar-hide'),
+    require("tailwind-scrollbar-hide"),
   ],
-}
-
+};
